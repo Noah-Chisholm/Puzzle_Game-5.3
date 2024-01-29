977bb4cc7ed53133045a4f8078b1fe32d15b7ad3
Noah-Chisholm <nchisholm1006@gmail.com>
2024-01-29 02:05:11 -0700

    general tweaks

---
+++ Source/Puzzle_Game/test_C_Class.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "test_C_Class.h"
+
+test_C_Class::test_C_Class()
+{
+}
+
+test_C_Class::~test_C_Class()
+{
+}
---
+++ Source/Puzzle_Game/test_C_Class.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PUZZLE_GAME_API test_C_Class
+{
+public:
+	test_C_Class();
+	~test_C_Class();
+};
